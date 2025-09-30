b3161f1570d8a2d40eb88a38aba5cdc003f5eea3
Madhavan R Mohan <madhavan.rajeswari.mohan@gmail.com>
2025-09-30 10:46:56 +0530

    added files via upload

--- js/glow.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-    const body = document.body;
-    // e.clientX and e.clientY give us the mouse's coordinates
-    // We update the CSS variables in real-time
-    body.style.setProperty('--mouse-x', `${e.clientX}px`);
-    body.style.setProperty('--mouse-y', `${e.clientY}px`);
-});
